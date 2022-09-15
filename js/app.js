ba9910abc54da439025102330df27b4bbaaac49a
滝口結 <[525tkg@gmail.com]>
2022-09-15 09:45:13 +0900

    ・モーダルでnot found 404エラー 　方向キーで解除 ・自己紹介導入　2sec ・自己紹介テキストアニメーション　9sec ・自己紹介タイムアップ

--- js/app.js
+++ js/app.js
@@ -1,13 +1,60 @@
-// グローバルIPアドレス取得
-// https://iwb.jp/javascript-ipinfo-jquery-axios/
+function self_introControl() {
+
+    // 自己紹介導入
+    $('.self-intro').each(function () {        
+        // 数秒後に実施 https://into-the-program.com/javascript-settimeout/
+        window.setTimeout(() => {$(this).addClass("self-introRemove")}, 3000); //3秒後に実行
+    });
+
+
+    // 自己紹介テキスト
+    $('.self-intro-txt').each(function () {        
+        // 数秒後に実施 https://into-the-program.com/javascript-settimeout/
+        window.setTimeout(() => {$(this).addClass("self-intro-txt_Show")}, 3000); //3秒後に実行
+        window.setTimeout(() => {$(this).removeClass("self-intro-txt_Show")}, 12000); //3秒後に実行
+    });
+    
+    // 自己紹介タイムアウト
+    $('.self-intro-timeout').each(function () {        
+        // 数秒後に実施 https://into-the-program.com/javascript-settimeout/
+        window.setTimeout(() => {$(this).addClass("self-intro-timeout_Show")}, 12000); //2秒後に実行
+    });
+    $('.fade-in').each(function () {        
+        // 数秒後に実施 https://into-the-program.com/javascript-settimeout/
+        window.setTimeout(() => {$(this).addClass("fade-inShow")}, 12000); //2秒後に実行
+    });
+
+
+}
+
+
+// ウィンドウ起動直後に実行する
 window.addEventListener("load", function(){
     // document.getElementById("message").innerHTML = "ページ読み込みが完了したよ！";
 
+    //コンソールログでチーターにメッセージ
+    console.log("ソースで ヒント 邪道")
 
-    fetch('https://ipinfo.io?callback')
-    .then(res => res.json())
-    .then(json => document.getElementById("ipaddress").innerHTML = json.ip)
+    document.getElementById("self-intro-sec").innerHTML = "５秒"
+
+
+    // モーダル https://myscreate.com/pure-modal/
+    $('#modalArea').fadeIn();
+
+
+    // モーダル x 方向キーでイベント実行
+    document.addEventListener(`keydown`, e => {
+        let output = ``;
+      
+        if (e.code == `ArrowDown` || e.code == `ArrowUp`) {
+            $('#modalArea').fadeOut();
+            self_introControl();
+
+        } else if (e.code == `ArrowLeft` || e.code == `ArrowRight`) {
+            $('#modalArea').fadeOut();
+            self_introControl();
+        }
+      
+      });
 
 });
-
-
